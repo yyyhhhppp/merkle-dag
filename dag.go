4af2796f2c76ec446ba7713a9e5d4936cf499966
yyyhhhppp <132680457+yyyhhhppp@users.noreply.github.com>
2024-03-05 19:42:22 +0800

    Update dag.go

--- dag.go
+++ dag.go
@@ -2,7 +2,17 @@ package merkledag
 
 import "hash"
 
-func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
-	return nil
+
+func Add(store KVStore, node Node, h crypto.Hash) []byte {
+    // 将Node中的数据保存在KVStore中
+    store.Write(node.Bytes())
+
+    // 计算Merkle Root
+    hf := h.New()
+    hf.Write(store.Read()) // 从KVStore中读取数据进行哈希计算
+    hashed := hf.Sum(nil)
+    fmt.Printf("Merkle Root: %x\n", hashed)
+
+    return hashed
 }
+
